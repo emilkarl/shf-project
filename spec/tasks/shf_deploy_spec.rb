7588765949113f04db2cd5e8fdbb77bdaff6d649
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-05-29 01:16:23 -0700

    if a task fails, it will be (re-)invoked the next time, til it succeeds

--- spec/tasks/shf_deploy_spec.rb
+++ spec/tasks/shf_deploy_spec.rb
@@ -11,17 +11,23 @@ RSpec.describe 'shf_deploy shf:deploy:run_onetime_tasks', type: :task do
   include_context 'simple rake task maker'
 
   let(:logfilepath) { LogfileNamer.name_for(OneTimeTasksFinder::SHFDEPLOY_LOG_NAME) }
-
-  let(:may_1_2019) { Time.parse('2019-05-01') }
-  let(:this_date_str) { may_1_2019.strftime("%Y-%m-%d") }
+  let(:simple_test_tasks_logfile) { LogfileNamer.name_for(SIMPLE_RAKE_TASK_LOGFILE) }
 
   before(:each) do
     File.delete(logfilepath) if File.file?(logfilepath)
+    File.delete(simple_test_tasks_logfile) if File.file?(simple_test_tasks_logfile)
   end
 
   after(:each) do
     File.delete(logfilepath) if File.file?(logfilepath)
+    File.delete(simple_test_tasks_logfile) if File.file?(simple_test_tasks_logfile)
   end
+
+
+  let(:may_1_2019) { Time.parse('2019-05-01') }
+  let(:may_29_2019) { Time.parse('2019-05-29') }
+  let(:may_1_str) { may_1_2019.strftime("%Y-%m-%d") }
+  let(:may_29_str) { may_29_2019.strftime("%Y-%m-%d") }
 
 
   context 'tasks found' do
@@ -43,9 +49,9 @@ RSpec.describe 'shf_deploy shf:deploy:run_onetime_tasks', type: :task do
 
           file_contents = File.exist?(logfilepath) ? File.read(logfilepath) : 'logfilepath does not exist'
           expect(file_contents).not_to include('ERROR')
-          expect(file_contents).to include("One-time task shf:test:task0 was run on #{this_date_str}")
-          expect(file_contents).to include("One-time task shf:test:task1 was run on #{this_date_str}")
-          expect(file_contents).to include("One-time task shf:test:task2 was run on #{this_date_str}")
+          expect(file_contents).to include("One-time task shf:test:task0 was run on #{may_1_str}")
+          expect(file_contents).to include("One-time task shf:test:task1 was run on #{may_1_str}")
+          expect(file_contents).to include("One-time task shf:test:task2 was run on #{may_1_str}")
         end
       end
 
@@ -56,24 +62,11 @@ RSpec.describe 'shf_deploy shf:deploy:run_onetime_tasks', type: :task do
           temp_onetime_path = Dir.mktmpdir('test-run_onetime_tasks')
           allow_any_instance_of(OneTimeTasksFinder).to receive(:onetime_tasks_path).and_return(temp_onetime_path)
 
-          # Create 1 .rake files in the 2019_Q2 directory
+          # Create 1 .rake file in the 2019_Q2 directory
           make_simple_rakefiles_under_subdir(temp_onetime_path, '2019_Q2', 1)
 
-          # Create 1 rake file that will cause an error:
-          taskname = "bad_task"
-          bad_task = "namespace :shf do\n" +
-              "  namespace :test do\n" +
-              "    desc 'bad task'\n" +
-              "    task #{taskname}: :environment do\n" +
-              "       puts \"  Task #{__FILE__} #{taskname} has been invoked.\"\n " +
-              "       raise NoMethodError\n" +
-              "    end\n" +
-              "  end\n" +
-              "end\n"
-          badtask_fn = File.join(temp_onetime_path, '2019_Q2', 'bad_task.rake')
-          File.open(badtask_fn, 'w') do |f|
-            f.puts bad_task
-          end
+          # Create 1 rake file that will raise an error unless it is run on 29 May 2019 (use Timecop!)
+          make_simple_rakefile_under_subdir(temp_onetime_path, '2019_Q2', 'bad_task', "raise NoMethodError unless DateTime.current.to_date == Date.new(2019, 5, 29)\n")
 
           # This should keep running and not raise an error just because 1 task fails; it should log the error(s)
           Timecop.freeze(may_1_2019) do
@@ -82,9 +75,44 @@ RSpec.describe 'shf_deploy shf:deploy:run_onetime_tasks', type: :task do
 
           file_contents = File.exist?(logfilepath) ? File.read(logfilepath) : 'logfilepath does not exist'
           expect(file_contents).to include("[error] Task shf:test:bad_task did not run successfully: NoMethodError")
-          expect(file_contents).to include("One-time task shf:test:task0 was run on #{this_date_str}")
+          expect(file_contents).to include("One-time task shf:test:task0 was run on #{may_1_str}")
+        end
+
+
+        it 'a task will be invoked next time if an error happened before' do
+
+          temp_onetime_path = Dir.mktmpdir('test-run_onetime_tasks')
+          allow_any_instance_of(OneTimeTasksFinder).to receive(:onetime_tasks_path).and_return(temp_onetime_path)
+
+          # Create 1 .rake file in the 2019_Q2 directory
+          make_simple_rakefiles_under_subdir(temp_onetime_path, '2019_Q2', 1)
+
+          # Create 1 rake file that will raise an error unless it is run on 29 May 2019 (use Timecop!)
+          make_simple_rakefile_under_subdir(temp_onetime_path, '2019_Q2', 'bad_task', "raise NoMethodError unless DateTime.current.to_date == Date.new(2019, 5, 29)\n")
+
+          # the 'bad_task will fail'
+          Timecop.freeze(may_1_2019) do
+            expect { subject.invoke }.not_to raise_error
+          end
+
+          file_contents = File.exist?(logfilepath) ? File.read(logfilepath) : 'logfilepath does not exist'
+          expect(file_contents).to include("[error] Task shf:test:bad_task did not run successfully: NoMethodError")
+          expect(file_contents).to include("One-time task shf:test:task0 was run on #{may_1_str}")
+
+
+          # The 'bad_task' should be invoked again and succeed
+          Timecop.freeze(may_29_2019) do
+            expect do
+              subject.reenable
+              subject.invoke
+            end.not_to raise_error
+          end
+
+          file_contents = File.exist?(logfilepath) ? File.read(logfilepath) : 'logfilepath does not exist'
+          expect(file_contents).to include("One-time task shf:test:bad_task was run on #{may_29_str}")
         end
       end
+
     end
   end
 

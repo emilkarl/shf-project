8b8a97c2aa2a1e0ca0dd2d1b31ffc4aa5a413149
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2021-03-31 21:00:29 -0700

    Membership renewed email (view, add to mailer method)

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -4,6 +4,7 @@ class MemberMailer < ApplicationMailer
 
 
   def membership_granted(member)
+
     set_mail_info __method__, member
     @member = member
     mail to: recipient_email, subject: t('mailers.member_mailer.membership_granted.subject')
@@ -13,24 +14,31 @@ class MemberMailer < ApplicationMailer
   def membership_renewed(member)
     set_mail_info __method__, member
     @member = member
+    @company = member.companies.first
+    @membership_last_day = member.membership_last_day
+    @companies = member.companies
+
     mail to: recipient_email, subject: t('mailers.member_mailer.membership_renewed.subject')
   end
 
 
   def membership_expiration_reminder(member)
-    @expire_date = member.membership_expire_date
+
     set_mail_info __method__, member
     @member      = member
+    @expire_date = member.membership_expire_date
     mail to:      @recipient_email,
          subject: t('mailers.member_mailer.membership_will_expire.subject')
+
   end
 
 
   def h_branding_fee_past_due(company, recipient)
-    @company = company
-    set_mail_info __method__, recipient
-    @member  = recipient
-    mail to: @recipient_email,  subject: t('mailers.member_mailer.h_branding_fee_past_due.subject')
+
+      set_mail_info __method__, recipient
+      @member  = recipient
+      @company = company
+      mail to: @recipient_email,  subject: t('mailers.member_mailer.h_branding_fee_past_due.subject')
   end
 
 

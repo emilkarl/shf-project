d3974b68c4aa6e00d432e8964be3021dbf9f47e6
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2021-04-04 15:56:17 -0700

    (minor) cuke payment steps: use specific factories; use Payment method, don't access class var directly

--- features/step_definitions/payment_steps.rb
+++ features/step_definitions/payment_steps.rb
@@ -17,7 +17,7 @@ Given(/^the following payments exist$/) do |table|
   end
 end
 
-# FIXME: this needs to call the process that updates (things) after a payment is made
+
 And(/^I complete the membership payment$/) do
   # Emulate webhook payment-update and direct to "success" action
 
@@ -33,8 +33,7 @@ And(/^I complete the membership payment$/) do
                               status: Payment.order_to_payment_status('successful'),
                               start_date: start_date, expire_date: expire_date)
 
-  # update the membership status
-  MembershipStatusUpdater.instance.payment_made(payment)
+  # The membership status is updated because a payment was made
   visit payment_success_path(user_id: @user.id, id: payment.id)
 end
 
@@ -46,8 +45,8 @@ And(/^I complete the branding payment for "([^"]*)"$/) do |company_name|
 
   start_date, expire_date = Company.next_branding_payment_dates(company.id)
 
-  payment = FactoryBot.create(:payment, user: @user, company: company,
-                              payment_type: 'branding_fee',
+  payment = FactoryBot.create(:h_branding_fee_payment,
+                              user: @user, company: company,
                               status: Payment.order_to_payment_status('successful'),
                               start_date: start_date, expire_date: expire_date)
 
@@ -65,7 +64,7 @@ And(/^I incur an error in payment processing$/) do
   unless payment
     user_id = @user.id
     start_date, expire_date = User.next_membership_payment_dates(user_id)
-    payment = Payment.create(payment_type: Payment::PAYMENT_TYPE_MEMBER,
+    payment = Payment.create(payment_type: Payment.membership_payment_type,
                               user_id: user_id,
                               company_id: nil,
                               status: Payment.order_to_payment_status(nil),
@@ -83,7 +82,7 @@ And(/^I incur an error in branding payment processing for "([^"]*)"$/) do |compa
   unless payment
     user_id = @user.id
     start_date, expire_date = Company.next_branding_payment_dates(company.id)
-    payment = Payment.create(payment_type: Payment::PAYMENT_TYPE_BRANDING,
+    payment = Payment.create(payment_type: Payment.branding_license_payment_type,
                              user_id: user_id,
                              company_id: company.id,
                              status: Payment.order_to_payment_status(nil),
